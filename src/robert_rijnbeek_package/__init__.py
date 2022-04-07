5bf291cfddbd254d949f55545d5ebb278e578407
R-Rijnbeek <robert270384@gmail.com>
2022-04-07 11:33:54 +0200

    finished the class, future improvement set some print result to make it easier to know on what method is it failing the process

--- src/robert_rijnbeek_package/__init__.py
+++ src/robert_rijnbeek_package/__init__.py
@@ -4,7 +4,7 @@ __init__.py case for test package!!
 """
 __author__  = "Robert Rijnbeek"
 __email__   = "robert270384@gmail.com"
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 
 def holaVersion():
